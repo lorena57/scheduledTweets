201fa6a3e1f5e1a573d7c5246cb34249f2800a96
Lorena C <lorena@eunknown.org>
2021-04-12 19:22:15 -0700

    create user errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,19 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
 
+    def create
+        params
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "successfully created account"
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
