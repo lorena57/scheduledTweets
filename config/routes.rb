ae476be23a594e9c69938983e67165ee1632ddfa
Lorena C <lorena@eunknown.org>
2021-04-12 21:06:48 -0700

    create logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get 'about', to: "about#index"
 
 get 'sign_up', to: 'registrations#new'
 post 'sign_up', to: 'registrations#create'
-delete 'logout' to: 'sessions#destroy'
+delete 'logout', to: 'sessions#destroy'
 
 root to: 'main#index'
 
