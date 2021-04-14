b3cd61d4081e16864f4268cb31cd4ad74b9d2e87
Lorena C <lorena@eunknown.org>
2021-04-14 13:20:24 -0700

    create mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,12 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
-        
-    end
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            redirect_to root_path, notice: 'If an account with that email was found, we have sent a link to rest your password.'
+            PasswordMailer.with(user: @user).reset.deliver_now
+        end
+    end 
 
 end
